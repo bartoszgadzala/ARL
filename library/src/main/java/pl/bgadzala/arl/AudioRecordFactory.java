6e821658586b468fa4e8a6ee25c02c605f88c50b
Bartosz Gadzała <bartosz.gadzala@gmail.com>
2012-07-27 21:23:11 +0200

    Created factor for AudioRecord.
    New factory for android.media.AudioRecord was created. It dramatically
    simplifies creation of best quality audio recorder available on a
    runtime platform.

---
+++ library/src/main/java/pl/bgadzala/arl/AudioRecordFactory.java
@@ -0,0 +1,50 @@
+package pl.bgadzala.arl;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
+/**
+ * Factory for audio recorder.
+ *
+ * @author Bartosz Gadzała
+ */
+public class AudioRecordFactory {
+
+    private static final int[] SAMPLE_RATES_IN_HZ = new int[]{44100, 22050, 16000, 11025, 8000};
+    private static final int[] CHANNEL_CONFIGS = new int[]{AudioFormat.CHANNEL_IN_STEREO, AudioFormat.CHANNEL_IN_MONO};
+    private static final int[] AUDIO_FORMATS = new int[]{AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_PCM_8BIT};
+
+    private AudioRecordFactory() {
+    }
+
+    /**
+     * Creates audio recorder for best quality which is available on runtime platform.
+     *
+     * @param audioSource source of audio (could be {@link MediaRecorder.AudioSource#MIC} for example)
+     *
+     * @return best available audio recorder or <code>null</code> if recorder can not be created
+     */
+    public static AudioRecord createForBestQuality(int audioSource) {
+        AudioRecord audioRecord = null;
+
+        for (int i = 0; i < SAMPLE_RATES_IN_HZ.length; i++) {
+            int sampleRateInHz = SAMPLE_RATES_IN_HZ[i];
+            for (int j = 0; j < CHANNEL_CONFIGS.length; j++) {
+                int channelConfig = CHANNEL_CONFIGS[j];
+                for (int k = 0; k < AUDIO_FORMATS.length; k++) {
+                    int audioFormat = AUDIO_FORMATS[k];
+                    int bufferSize = AudioRecord.getMinBufferSize(sampleRateInHz, channelConfig, audioFormat);
+                    if (bufferSize > 0) {
+                        audioRecord = new AudioRecord(audioSource, sampleRateInHz, channelConfig, audioFormat, bufferSize);
+                        if (audioRecord.getState() == AudioRecord.STATE_INITIALIZED) {
+                            return audioRecord;
+                        }
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
