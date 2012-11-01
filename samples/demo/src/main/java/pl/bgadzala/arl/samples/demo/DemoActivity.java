0f4006c4a5fe451635a123c8c6f47a82655d7b4d
Bartosz Gadzała <bartosz.gadzala@gmail.com>
2012-11-01 11:34:28 +0100

    Demo skeleton.
    Created skeleton of Demo application.

---
+++ samples/demo/src/main/java/pl/bgadzala/arl/samples/demo/DemoActivity.java
@@ -0,0 +1,17 @@
+package pl.bgadzala.arl.samples.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: bartek
+ * Date: 11/1/12
+ * Time: 11:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DemoActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
