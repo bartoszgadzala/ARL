fd3e51b63e3a28e3d20167c7d721db430c434b27
Bartosz Gadzała <bartosz.gadzala@gmail.com>
2012-11-01 13:38:06 +0100

    Created skeleton of demo application.
    New layout for demo application has been created.

--- samples/demo/src/main/java/pl/bgadzala/arl/samples/demo/DemoActivity.java
+++ samples/demo/src/main/java/pl/bgadzala/arl/samples/demo/DemoActivity.java
@@ -3,15 +3,15 @@ package pl.bgadzala.arl.samples.demo;
 import android.app.Activity;
 import android.os.Bundle;
 
+
 /**
- * Created with IntelliJ IDEA.
- * User: bartek
- * Date: 11/1/12
- * Time: 11:13 AM
- * To change this template use File | Settings | File Templates.
+ * Launcher activity.
  */
 public class DemoActivity extends Activity {
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
     }
+
 }
